c0a29f07f86afedba7f265ca68ef4c54865fc912
Chase Hashiguchi <Chasehashigu@gmail.com>
2025-01-23 09:36:30 -0600

    Initial Commit

---
+++ slidingPuzzle/slidingPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  slidingPuzzleApp.swift
+//  slidingPuzzle
+//
+//  Created by Chase Hashiguchi on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct slidingPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ slidingPuzzleTests/slidingPuzzleTests.swift
@@ -0,0 +1,16 @@
+//
+//  slidingPuzzleTests.swift
+//  slidingPuzzleTests
+//
+//  Created by Chase Hashiguchi on 1/23/25.
+//
+
+import Testing
+
+struct slidingPuzzleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
